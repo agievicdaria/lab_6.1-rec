99c3e56009d07629d6952f2fa499eda6e6bf8803
User0809 <agievicdaria@gmail.com>
2023-11-13 10:51:46 +0200

    update

--- lab_6.1-rec.cpp
+++ lab_6.1-rec.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
 
 using namespace std;
 
@@ -8,28 +9,33 @@ const int ARRAY_SIZE = 20;
 const int LOWER_BOUND = -20;
 const int UPPER_BOUND = 50;
 
-void generateArray(int arr[], int size, int lower, int upper, int i) {
+void generateArray(int arr[], int size, int lower, int upper, int i)
+{
     arr[i] = lower + rand() % (upper - lower + 1);
-    if (i < size - 1) {
+    if (i < size - 1)
+    {
         generateArray(arr, size, lower, upper, i + 1);
     }
 }
 
-void printArray(int arr[], int size, int index = 0) {
+void printArray(int a[], int size, int index = 0) {
     if (index < size) {
-        cout << arr[index] << " ";
-        printArray(arr, size, index + 1);
+        cout << setw(6) << a[index] << " ";
+        printArray(a, size, index + 1);
     } else {
         cout << endl;
     }
 }
 
-void calculateSumAndChangeToZero(int arr[], int size, int index, int &count, int &sum) {
-    if (index >= size) {
+void calculateSumAndChangeToZero(int arr[], int size, int index, int &count, int &sum)
+{
+    if (index >= size)
+    {
         return;
     }
 
-    if (arr[index] > 0 && index % 5 != 0) {
+    if (arr[index] > 0 && !(index % 5 == 0))
+    {
         count++;
         sum += arr[index];
         arr[index] = 0;
@@ -38,7 +44,8 @@ void calculateSumAndChangeToZero(int arr[], int size, int index, int &count, int
     calculateSumAndChangeToZero(arr, size, index + 1, count, sum);
 }
 
-int main() {
+int main()
+{
     srand((unsigned)time(NULL));
 
     int array[ARRAY_SIZE];
@@ -46,6 +53,7 @@ int main() {
 
     cout << "Оригінальний масив: ";
     printArray(array, ARRAY_SIZE);
+    cout << endl;
 
     int count = 0;
     int sum = 0;
@@ -56,6 +64,7 @@ int main() {
 
     cout << "Модифікований масив: ";
     printArray(array, ARRAY_SIZE);
+    cout << endl;
 
     return 0;
 }
